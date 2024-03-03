608c61ff669bf2a51203f978b2cdfbc8677be829
nkuzmischev <nkuzmischev@gmail.com>
2024-03-03 16:39:10 +0300

    p.16-17

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -14,49 +14,44 @@ import java.util.List;
 public class LogicServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Получаем текущую сессию
+
         HttpSession currentSession = req.getSession();
 
-        // Получаем объект игрового поля из сессии
+
         Field field = extractField(currentSession);
 
-        // получаем индекс ячейки, по которой произошел клик
+
         int index = getSelectedIndex(req);
         Sign currentSign = field.getField().get(index);
 
-        // Проверяем, что ячейка, по которой был клик пустая.
-        // Иначе ничего не делаем и отправляем пользователя на ту же страницу без изменений
-        // параметров в сессии
+
         if (Sign.EMPTY != currentSign) {
             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
             dispatcher.forward(req, resp);
             return;
         }
 
-        // ставим крестик в ячейке, по которой кликнул пользователь
+
         field.getField().put(index, Sign.CROSS);
         if (checkWin(resp, currentSession, field)) {
             return;
         }
 
 
-        // Получаем пустую ячейку поля
         int emptyFieldIndex = field.getEmptyFieldIndex();
 
         if (emptyFieldIndex >= 0) {
             field.getField().put(emptyFieldIndex, Sign.NOUGHT);
-            // Проверяем, не победил ли нолик после добавление последнего нолика
+
             if (checkWin(resp, currentSession, field)) {
                 return;
             }
         }
 
 
-
-        // Считаем список значков
         List<Sign> data = field.getFieldData();
 
-        // Обновляем объект поля и список значков в сессии
+
         currentSession.setAttribute("data", data);
         currentSession.setAttribute("field", field);
 
@@ -81,16 +76,12 @@ public class LogicServlet extends HttpServlet {
     private boolean checkWin(HttpServletResponse response, HttpSession currentSession, Field field) throws IOException {
         Sign winner = field.checkWin();
         if (Sign.CROSS == winner || Sign.NOUGHT == winner) {
-            // Добавляем флаг, который показывает что кто-то победил
             currentSession.setAttribute("winner", winner);
 
-            // Считаем список значков
             List<Sign> data = field.getFieldData();
 
-            // Обновляем этот список в сессии
             currentSession.setAttribute("data", data);
 
-            // Шлем редирект
             response.sendRedirect("/index.jsp");
             return true;
         }
