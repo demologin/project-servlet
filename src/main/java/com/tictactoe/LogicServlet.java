f453ff274b3630ae2dc8881d4b769ca7a8ac90a2
Egor Kondrashov <erop.kondrashov@yandex.ru>
2024-03-08 21:02:24 +0400

    add jstl tag to index.jsp and change square numbers to X when clicking

---
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -0,0 +1,46 @@
+package com.tictactoe;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "LogicServlet", value = "/logic")
+public class LogicServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession currentSession = req.getSession();
+        Field field = extractField(currentSession);
+
+        int index = getSelectedIndex(req);
+        field.getField().put(index, Sign.CROSS);
+
+        List<Sign> data = field.getFieldData();
+
+        currentSession.setAttribute("data", data);
+        currentSession.setAttribute("field", field);
+
+        resp.sendRedirect("/index.jsp");
+    }
+
+    private int getSelectedIndex(HttpServletRequest request) {
+        String click = request.getParameter("click");
+        boolean isNumeric = click.chars().allMatch(Character::isDigit);
+        return isNumeric ? Integer.parseInt(click) : 0;
+    }
+
+    private Field extractField(HttpSession currentSession) {
+        Object fieldAttribute = currentSession.getAttribute("field");
+        if (Field.class != fieldAttribute.getClass()) {
+            currentSession.invalidate();
+            throw new RuntimeException("Something's wrong, try one more time");
+        }
+        return (Field) fieldAttribute;
+    }
+}
